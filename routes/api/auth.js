75413016e60d8679ed5574357387f43453ae997d
AhmedKamal199 <akm.kamal123456@gmail.com>
2022-03-10 10:21:20 +0200

    Add Jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,60 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const jwt = require("jsonwebtoken");
 const axios = require("axios");
-router.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const User = require("../../models/User");
+const bcrypt = require("bcrypt");
+const jwt_s = process.env.JWT_SECRET;
+require("dotenv").config();
 
+router.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400).json({ msg: "Please Enter all filed" });
+  }
+  User.findOne({ email }).then(user => {
+    if (user) res.status(400).json({ msg: "user already exits" });
+  });
+  const NewUser = new User({
+    name,
+    email,
+    password
+  });
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(NewUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      NewUser.password = hash;
+      NewUser.save().then(user => {
+        const token = jwt.sign({ id: user.id }, jwt_s, {
+          expiresIn: 3600
+        });
+
+        res.json({
+          token,
+          user: {
+            id: user.id,
+            name: user.name,
+            email: user.email
+          }
+        });
+      });
+    });
+  });
+});
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).json({ msg: "Please Enter all fileds" });
+  }
+  User.findOne({ email }).then(user => {
+    if (!user) res.status(404).json({ msg: "This user is not exits" });
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (!isMatch) res.status(401).json({ msg: "Invalid Crediential" });
+    });
+    const token = jwt.sign({ id: user.id }, jwt_s, { expiresIn: 3600 });
+    res.status(200).json({ token, msg: "Done" });
+  });
+});
 const clientId = "5c369149d93c699d4b4a";
 const clientSecrets = "fd17b96b8ead4f115e10862d2ac5b8181750b8a5";
 let token = null;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongo = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
 app.use(cors("*"));
-
+app.use(bodyParser.json());
 app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/hel", require("./routes/api/hel"));
 const port = process.env.PORT || 5000;
 
 const run = async () => {
