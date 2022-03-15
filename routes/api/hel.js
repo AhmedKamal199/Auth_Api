92f1642e0167ca34f03fca92f9c74897230780dd
AhmedKamal199 <akm.kamal123456@gmail.com>
2022-03-15 22:02:42 +0200

    Add middle to get request

--- routes/api/hel.js
+++ routes/api/hel.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const auth = require("../../midlewares/auth");
 router.get("/", auth, (req, res) => {
-  res.send("Hello World");
+  res.send(`Hello ${user.name}`);
 });
 
 module.exports = router;
