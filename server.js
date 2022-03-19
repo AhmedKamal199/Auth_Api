e7dfd749a2e0bf67812dab00ad119fbc6a7b4b86
AhmedKamal199 <akm.kamal123456@gmail.com>
2022-03-19 22:13:52 +0200

    Add passport configuration

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongo = require("mongoose");
 const cors = require("cors");
+const passport = require("passport")
+// Gentral Config
 require("dotenv").config();
 const app = express();
 
 app.use(cors("*"));
 app.use(bodyParser.json());
+
+// Middlewares
+
+app.use(passport.initialize());
+
+
+// Routes
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/hel", require("./routes/api/hel"));
+
+//Start server
 const port = process.env.PORT || 5000;
 
 const run = async () => {
